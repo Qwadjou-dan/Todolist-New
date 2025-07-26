ca72f2612a0ce0954a3d34324db1bd68be73eae3
Daniel Owusu <owusudaniel077@gmail.com>
2025-07-26 00:21:49 +0000

    Update index.js
    Url updated to allow smooth deployment. Middleware also updated

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,24 @@ require("dotenv").config();
 const todolistApp = express();
 const PORT = process.env.PORT || 1001;
 
+//Allowed url
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://todolist-frontend-5gh3.onrender.com"
+];
+
 //middleware
 todolistApp.use(bodyParser.json());
 todolistApp.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
